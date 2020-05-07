cb74e1a000d3257b90b0443eb4b0301e7873bf88
Jackie61 <62676180+Jackie61@users.noreply.github.com>
2020-05-06 19:40:52 -0700

    Add files via upload

---
+++ JAVASCRIP and Json/first.js
@@ -0,0 +1,8 @@
+//first web app 
+function timeFunction()
+{
+    var timerstart = Date();
+    document.getElementById('startTime').innerHTML = timerstart;
+    document.getElementById('TimeTake').innerHTML = Date();
+}
+
