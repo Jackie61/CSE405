6cb73bdbd021679c967a4cd4ffa6eca8bfe6ac3e
Jackie61 <62676180+Jackie61@users.noreply.github.com>
2020-05-27 18:16:00 -0700

    Add files via upload

--- Public/anylogin.js
+++ Public/anylogin.js
@@ -1,67 +1,90 @@
-document.addEventListener("DOMContentLoaded" , event =>{
-    const app = firebase.app();
-    console.log(app);
-})
-
+document.addEventListener('DOMContentLoaded' , event =>{
+  const app = firebase.app();
+  console.log(app);
+});
+var currentUser = null;
 function googleLogin()
 {
-    const provider = new firebase.auth.GoogleAuthProvider();
-    firebase.auth().signInWithPopup(provider)
-        .then(result => {
-            const user = result.user;
-           var name = user.displayName;
-           document.getElementById('Name').innerHTML = name;
-           
-           console.log(user)
-        }).catch(console.log)
-        firebase.auth().setPersistence(firebase.auth.Auth.Persistence.SESSION)
-        .then(function() {
-          // Existing and future Auth states are now persisted in the current
-          // session only. Closing the window would clear any existing state even
-          // if a user forgets to sign out.
-          // ...
-          // New sign-in will be persisted with session persistence.
-        })
-        .catch(function(error) {
-          // Handle Errors here.
-          var errorCode = error.code;
-          var errorMessage = error.message;
-        });
-}
-
+  const provider = new firebase.auth.GoogleAuthProvider();
+  firebase.auth().signInWithPopup(provider)
+      .then(result => {
+        const currentUser = result.user;
+         var name = currentUser.displayName;
+         document.getElementById('Name').innerHTML = name;
+         
+         console.log(user);
+      }).authStateChanged(currentUser)
+      firebase.auth().setPersistence(firebase.auth.Auth.Persistence.LOCAL)
+      .then(function() {
+      // Existing and future Auth states are now persisted in the current
+      // session only. Closing the window would clear any existing state even
+      // if a user forgets to sign out.
+      // ...
+      // New sign-in will be persisted with session persistence.
+      })
+      .catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+      });
+};
 
 function Logout()
 {
-    const btnLogout = document.getElementById('logout');
-    btnLogout.addEventListener('click', e => {
-        firebase.auth().signOut();
-        console.log("signed out")
-    });
-
-}
-function AnyonymousLogin(){
-    //get element
-    const btnLogin = document.getElementById('Anyonymouslogin');
-
-    btnLogin.addEventListener('click', e => {
-        firebase.auth().signInAnonymously();
-    });
-
-    firebase.auth().onAuthStateChanged(firebaseUser => {
-        console.log(firebaseUser);
-    });
-    firebase.auth().setPersistence(firebase.auth.Auth.Persistence.SESSION)
-  .then(function() {
-    // Existing and future Auth states are now persisted in the current
-    // session only. Closing the window would clear any existing state even
-    // if a user forgets to sign out.
-    // ...
-    // New sign-in will be persisted with session persistence.
-  })
-  .catch(function(error) {
-    // Handle Errors here.
-    var errorCode = error.code;
-    var errorMessage = error.message;
+  const btnLogout = document.getElementById('logout');
+  btnLogout.addEventListener('click', e => {
+      firebase.auth().signOut();
+      console.log("signed out")
   });
+  currentUser = null;
 
 };
+
+function AnyonymousLogin(){
+  //get element
+  const btnLogin = document.getElementById('Anyonymouslogin');
+
+  btnLogin.addEventListener('click', se => {
+      firebase.auth().signInAnonymously().then(result => {
+        const currentUser = result.user;
+       var name = currentUser.uid;
+       document.getElementById('Name').innerHTML = name;
+       
+       console.log(user)
+    }).authStateChanged(currentUser)
+  });
+
+  firebase.auth().setPersistence(firebase.auth.Auth.Persistence.LOCAL)
+.then(function() {
+  // Existing and future Auth states are now persisted in the current
+  // session only. Closing the window would clear any existing state even
+  // if a user forgets to sign out.
+  // ...
+  // New sign-in will be persisted with session persistence.
+})
+.catch(function(error) {
+  // Handle Errors here.
+  var errorCode = error.code;
+  var errorMessage = error.message;
+});//firebase code
+
+};
+
+function authStateChanged(users) {
+if (users === null) {
+  messageS.innerHTML            = "logged out"
+  GoogleLogin.style.visibility  = "visible"
+  AnyonyLogin.style.visibility   = "visible"
+  userPage.style.display     = "none"
+} else {
+  messageS.innerHTML            = "logged in"
+  GoogleLogin.style.visibility  = "hidden"
+  AnyonyLogin.style.visibility   = "hidden"
+  userPage.style.visibility     = "inline"
+
+}
+}
+
+
+
+
--- Public/app.js
+++ Public/app.js
@@ -1,5 +1,78 @@
-
-
-
-
-
+document.addEventListener('DOMContentLoaded' , event =>{
+    const app = firebase.app();
+    console.log(app);
+  });
+  var currentUser = true;
+  function googleLogin()
+  {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    firebase.auth().signInWithPopup(provider)
+        .then(result => {
+          const currentUser = result.user;
+           console.log(currentUser);
+        })
+        firebase.auth().setPersistence(firebase.auth.Auth.Persistence.LOCAL)
+        .then(function() {
+        // Existing and future Auth states are now persisted in the current
+        // session only. Closing the window would clear any existing state even
+        // if a user forgets to sign out.
+        // ...
+        // New sign-in will be persisted with session persistence.
+        })
+        .catch(function(error) {
+          // Handle Errors here.
+          var errorCode = error.code;
+          var errorMessage = error.message;
+        });
+  };
+  
+  function Logout()
+  {
+    const btnLogout = document.getElementById('logout');
+    btnLogout.addEventListener('click', e => {
+        firebase.auth().signOut();
+        console.log("signed out")
+    });  
+  };
+  
+  function AnyonymousLogin(){
+    //get element
+    const btnLogin = document.getElementById('Anyonymouslogin');
+  
+    btnLogin.addEventListener('click', event => {
+        firebase.auth().signInAnonymously().then(result => {
+          const currentUser = result.user;
+         console.log(currentUser)
+      })
+    });
+    firebase.auth().setPersistence(firebase.auth.Auth.Persistence.LOCAL)
+  .then(function() {
+    // Existing and future Auth states are now persisted in the current
+    // session only. Closing the window would clear any existing state even
+    // if a user forgets to sign out.
+    // ...
+    // New sign-in will be persisted with session persistence.
+  })
+  .catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+  });//firebase code
+  
+  };
+  
+  function authStateChanged(users) {
+  if (users === null) {
+    messageS.innerHTML            = "logged out"
+    GoogleLogin.style.visibility  = "visible"
+    AnyonyLogin.style.visibility   = "visible"
+    userPage.style.display     = "none"
+  } else {
+    messageS.innerHTML            = "logged in"
+    GoogleLogin.style.visibility  = "hidden"
+    AnyonyLogin.style.visibility   = "hidden"
+    userPage.style.visibility     = "inline"
+  
+  }
+  }
+  
